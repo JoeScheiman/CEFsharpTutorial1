81ccf0cef0c44166d5c641f0a9bb54da704ec477
Joe Scheiman <scheiman.joe@gmail.com>
2019-05-03 18:19:51 -0400

    first commit - app works

---
+++ README.md
@@ -0,0 +1,1 @@
+# CEFsharp Tutorial 1
---
+++ Sandbox/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Sandbox
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // At the initialization, start chromium
+
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            // Initialize Cef with the provided settings
+            Cef.Initialize(settings);
+
+            // Create a browser component
+            // and provide a start URL
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+
+            // Add the browser to the form
+            this.Controls.Add(chromeBrowser);
+            // Make the browser fill the form
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
